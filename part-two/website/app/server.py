bffb109e18c15ece5ddb51bc96541c138978c9a1
Kat Cosgrove <kat.cosgrove@gmail.com>
2021-12-20 10:13:26 -0800

    dockerized website

---
+++ part-two/website/app/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template("portfolio.html")
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True) 
